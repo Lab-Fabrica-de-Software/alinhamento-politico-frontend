c7992198126cd64d979b0f61f7113ce38a7e084e
renan-mion <rnunesmion@gmail.com>
2025-04-15 16:07:39 -0300

    Added Click/Change functions and style changes in NavbarComponent

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,8 +8,28 @@ import { User } from "../../core/models/user";
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  logo: string = "";
+  logo = "";
+  searchInput = "";
 
   @Input()
   user: User = {} as User;
+
+  search() {
+    console.log(this.searchInput);
+  }
+
+  onRedirectNotifications () {
+    window.location.href = "/notifications";
+  }
+
+  onSelectOption(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const page = target.value;
+
+    if (!page) {
+      return;
+    }
+
+    window.location.href = page;
+  }
 }

1bde11cb08c989c9c1e61c81ed51065f5011af01
renan-mion <rnunesmion@gmail.com>
2025-04-29 19:40:56 -0300

    ✨ feat: create form and textarea

--- src/app/modules/debate/components/debate-contribution/debate-contribution.stories.ts
+++ src/app/modules/debate/components/debate-contribution/debate-contribution.stories.ts
@@ -1,11 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { DebateContributionComponent } from './debate-contribution.component';
+import { FormsModule } from '@angular/forms';
 
 const meta: Meta<DebateContributionComponent> = {
   title: 'modules/debate/components/debate-contribution',
   component: DebateContributionComponent,
   excludeStories: /.*Data$/,
   tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [FormsModule]
+    })
+  ]
 };
 
 export default meta;
--- src/app/modules/debate/debate.module.ts
+++ src/app/modules/debate/debate.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DebateRoutingModule } from './debate-routing.module';
 import { HomeComponent } from './pages/home/home.component';
@@ -15,7 +16,8 @@ import { DebateContributionComponent } from './components/debate-contribution/de
   ],
   imports: [
     CommonModule,
-    DebateRoutingModule
+    DebateRoutingModule,
+    FormsModule
   ]
 })
 export class DebateModule { }

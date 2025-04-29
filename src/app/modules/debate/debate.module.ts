8a22ec246fb7358a74f1f3fb460b5d587d07e077
renan-mion <rnunesmion@gmail.com>
2025-04-29 13:19:03 -0300

    ♻️ refactor: Move SelectionDateRange component to shared module

--- src/app/modules/debate/debate.module.ts
+++ src/app/modules/debate/debate.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { DebateRoutingModule } from './debate-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { DebateGroupComponent } from './components/debate-group/debate-group.component';
-import { SelectionDateRangeComponent } from './components/selection-date-range/selection-date-range.component';
+import { SelectionDateRangeComponent } from '../../shared/selection-date-range/selection-date-range.component';
 
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
--- src/app/shared/selection-date-range/selection-date-range.stories.ts
+++ src/app/shared/selection-date-range/selection-date-range.stories.ts
@@ -3,10 +3,10 @@ import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { SelectionDateRangeComponent } from './selection-date-range.component';
 import { NgbDateParserFormatter, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { PtBrDateParserFormatter } from '../../../../core/shared/formatters/pt-br-date-parser-formatter';
+import { PtBrDateParserFormatter } from '../../core/shared/formatters/pt-br-date-parser-formatter';
 
 const meta: Meta<SelectionDateRangeComponent> = {
-  title: 'SelectionDateRange',
+  title: 'modules/shared/components/selectionDateRange',
   component: SelectionDateRangeComponent,
   //👇 Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,

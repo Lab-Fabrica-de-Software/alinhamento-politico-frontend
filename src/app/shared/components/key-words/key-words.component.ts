7538120844fc6eeb82c2d010722ac6accefa0a57
renan-mion <rnunesmion@gmail.com>
2025-05-05 16:08:14 -0300

    ✨ feat: add marking category functions

--- src/app/shared/components/key-words/key-words.component.ts
+++ src/app/shared/components/key-words/key-words.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Category } from '../../../core/models/category';
 
 @Component({
@@ -8,5 +8,28 @@ import { Category } from '../../../core/models/category';
   styleUrl: './key-words.component.css'
 })
 export class KeyWordsComponent {
+  @Input()
   categories: Category[] = [];
+
+  markedCategories: Category[] = [];
+
+  addMarkedCategory(category: Category) {
+    this.markedCategories.push(category);
+  }
+
+  removeMarkedCategory(category: Category) {
+    this.markedCategories = this.markedCategories.filter((c) => c !== category);
+  }
+
+  toggleCategory(category: Category) {
+    if (this.isCategoryMarked(category)) {
+      this.removeMarkedCategory(category);
+    } else if (this.markedCategories.length < 5) {
+      this.addMarkedCategory(category);
+    }
+  }
+  
+  isCategoryMarked(category: Category): boolean {
+    return this.markedCategories.includes(category);
+  }
 }

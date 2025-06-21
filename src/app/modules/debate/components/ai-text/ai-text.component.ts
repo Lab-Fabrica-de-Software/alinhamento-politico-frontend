fe714a81f5d2698f1a4ce93603bd3939cf14f89e
Renan Mion <rnunesmion@gmail.com>
2025-06-21 15:17:02 -0300

    ✨ feat: add Input for aiFeedback

--- src/app/modules/debate/components/ai-text/ai-text.component.ts
+++ src/app/modules/debate/components/ai-text/ai-text.component.ts
@@ -8,24 +8,36 @@ import { AiFeedback } from '../../../../core/models/ai-feedback';
   templateUrl: './ai-text.component.html',
   styleUrl: './ai-text.component.css'
 })
-export class AiTextComponent {
+export class AiTextComponent implements OnInit {
   @Input() aiText: AiText | null = null;
+  @Input() aiFeedback: AiFeedback | null = null;
 
   @Output() aiFeedbackChange = new EventEmitter<AiFeedback | null>();
 
   showModal: boolean = false;
-  aiFeedback: AiFeedback = {
-    id: 1,
-    aiTextId: 1,
-    feedback: null,
-    text: ''
-  };
+
+  ngOnInit(): void {
+    if (!this.aiFeedback && this.aiText) {
+      this.aiFeedback = {
+        id: 0,
+        aiTextId: this.aiText.id,
+        feedback: null,
+        text: ''
+      };
+    }
+  }
 
   get charCount(): number {
-    return this.aiFeedback.text?.length ?? 0;
+    return this.aiFeedback?.text?.length ?? 0;
+  }
+
+  get hasFeedback(): boolean {
+    return this.aiFeedback?.feedback !== null;
   }
 
   setLike(value: boolean) {
+    if (!this.aiFeedback || this.hasFeedback) return;
+
     if (value) {
       this.aiFeedback.feedback = true;
       this.aiFeedback.text = '';
@@ -42,12 +54,21 @@ export class AiTextComponent {
   }
 
   sendFeedback() {
+    if (!this.aiFeedback) return;
     this.aiFeedback.feedback = false;
     this.aiFeedbackChange.emit({ ...this.aiFeedback });
     this.closeModal();
   }
 
+  resetFeedback() {
+    if (!this.aiFeedback) return;
+    this.aiFeedback.feedback = null;
+    this.aiFeedback.text = '';
+    this.aiFeedbackChange.emit({ ...this.aiFeedback });
+  }
+
   closeModal() {
+    if (!this.aiFeedback) return;
     this.aiFeedback.text = '';
     this.showModal = false;
   }
